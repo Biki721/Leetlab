736464db33bff76a3f04859a82b9a07465a5372c
Biki Dey <63406490+Biki721@users.noreply.github.com>
2025-08-24 11:48:30 +0000

    add asyncHandler, user prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+
 const port = process.env.PORT || 8000;
 
 app.get("/health", (req, res) => {
